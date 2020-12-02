ce94b00b2b81b87f5560d1259bd6af595e5c6427
KoknaevD <89024464050@yandex.ru>
2020-12-02 21:49:35 +0500

    done

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Dima K")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+        binding.apply {
+            doneButton.setOnClickListener {
+                myName?.nickname = this.nicknameEdit.text.toString()
+                this.invalidateAll()
+                this.nicknameEdit.visibility = View.GONE
+                it.visibility = View.GONE
+                this.nicknameText.visibility = View.VISIBLE
+
+                // Hide the keyboard.
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(it.windowToken, 0)
+            }
+        }
+
     }
 }
